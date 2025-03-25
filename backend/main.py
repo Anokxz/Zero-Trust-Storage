b16f94428329c9e4079624fa376935cce9709a8f
Anokxz <snakshayan2@gmail.com>
2025-03-25 06:30:44 +0000

    Removed Duplicate auth function
    - Removed the login and register

--- backend/main.py
+++ backend/main.py
@@ -22,33 +22,12 @@ app.add_middleware(
 
 Base.metadata.create_all(bind=engine)
 
-# app.include_router(auth.router, prefix="/auth")
-# app.include_router(users.router, prefix="/auth", tags=["Authentication"])
-app.include_router(files.router, prefix="/files")
+app.include_router(users.router, prefix="/user", tags=["Authentication"])
+app.include_router(files.router, prefix="/files", tags=["File Handling"])
 
 @app.get("/")
 def root():
     return {"message": "Secure File Storage API is running!"}
-
-# @app.post("/register", response_model=UserResponse)
-# def register(user: UserCreate, db: Session = Depends(get_db)):
-#     existing_user = db.query(User).filter(User.email == user.email).first()
-#     if existing_user:
-#         raise HTTPException(status_code=400, detail="Email already registered")
-#     hashed_password = get_password_hash(user.password)
-#     new_user = User(email=user.email, hashed_password=hashed_password)
-#     db.add(new_user)
-#     db.commit()
-#     db.refresh(new_user)
-#     return new_user
-
-# @app.post("/login")
-# def login(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.email == user.email).first()
-    if not db_user or not verify_password(user.password, db_user.hashed_password):
-        raise HTTPException(status_code=401, detail="Invalid credentials")
-    token = create_access_token({"sub": db_user.email})
-    return {"access_token": token, "token_type": "bearer"}
 
 if __name__ == "__main__":
     pass
